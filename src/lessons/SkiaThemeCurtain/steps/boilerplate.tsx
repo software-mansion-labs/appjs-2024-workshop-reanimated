d381c5d5149a9869b352b38e6ba9b67c001c719c
kacperkapusciak <kacper.kapusciak@swmansion.com>
2024-05-17 12:46:42 +0200

    Implement boilerplate

---
+++ src/lessons/SkiaThemeCurtain/steps/boilerplate.tsx
@@ -0,0 +1,163 @@
+import { useEffect } from "react";
+import {
+  Appearance,
+  Dimensions,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from "react-native";
+
+import { Cards } from "@/components/Cards";
+import { SearchBar } from "@/components/SearchBar";
+import { Trending } from "@/components/Trending";
+
+import { Transition, glsl } from "@/lib/shader";
+import { StatusBar } from "expo-status-bar";
+
+const TRANSITION_DURATION = 800;
+
+const { width, height } = Dimensions.get("window");
+
+const warpUp: Transition = glsl`
+// Author: pschroen
+// License: MIT
+
+const vec2 direction = vec2(0.0, -1.0);
+
+const float smoothness = 0.5;
+const vec2 center = vec2(0.5, 0.5);
+
+vec4 transition (vec2 uv) {
+  vec2 v = normalize(direction);
+  v /= abs(v.x) + abs(v.y);
+  float d = v.x * center.x + v.y * center.y;
+  float m = 1.0 - smoothstep(-smoothness, 0.0, v.x * uv.x + v.y * uv.y - (d - 0.5 + progress * (1.0 + smoothness)));
+  return mix(getFromColor((uv - 0.5) * (1.0 - m) + 0.5), getToColor((uv - 0.5) * m + 0.5), m);
+}
+`;
+
+const warpDown: Transition = glsl`
+// Author: pschroen
+// License: MIT
+
+const vec2 direction = vec2(0.0, 1.0);
+
+const float smoothness = 0.5;
+const vec2 center = vec2(0.5, 0.5);
+
+vec4 transition (vec2 uv) {
+  vec2 v = normalize(direction);
+  v /= abs(v.x) + abs(v.y);
+  float d = v.x * center.x + v.y * center.y;
+  float m = 1.0 - smoothstep(-smoothness, 0.0, v.x * uv.x + v.y * uv.y - (d - 0.5 + progress * (1.0 + smoothness)));
+  return mix(getFromColor((uv - 0.5) * (1.0 - m) + 0.5), getToColor((uv - 0.5) * m + 0.5), m);
+}
+`;
+
+export function SkiaThemeCurtain() {
+  const colorScheme = useColorScheme();
+  const changeTheme = () => {
+    Appearance.setColorScheme(colorScheme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const listener = Appearance.addChangeListener(() => {
+      console.log('theme changed!')
+    });
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+
+  return (
+    <View style={styles.fill}>
+      <ScrollView
+        style={[
+          styles.container,
+          { height: height },
+          colorScheme === "light"
+            ? { backgroundColor: "white" }
+            : { backgroundColor: "#020617" },
+        ]}
+      >
+        <View
+          style={[
+            { width: width },
+            colorScheme === "light"
+              ? { backgroundColor: "white" }
+              : { backgroundColor: "#020617" },
+          ]}
+        >
+          <View style={styles.padding}>
+            <View style={styles.row}>
+              <Text
+                style={[
+                  styles.header,
+                  colorScheme === "light"
+                    ? { color: "#0f172a" }
+                    : { color: "#f1f5f9" },
+                ]}
+              >
+                Home
+              </Text>
+              <Pressable style={styles.themeSwitcher} onPress={changeTheme}>
+                <Text
+                  style={
+                    colorScheme === "light"
+                      ? { color: "#0f172a" }
+                      : { color: "#f1f5f9" }
+                  }
+                >
+                  Switch theme
+                </Text>
+              </Pressable>
+            </View>
+            <SearchBar />
+            <Trending />
+            <Cards />
+          </View>
+        </View>
+      </ScrollView>
+      <StatusBar translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "ios" ? 50 : 10,
+  },
+  fill: {
+    flex: 1,
+  },
+  padding: {
+    padding: 16,
+  },
+  overlay: {
+    position: "absolute",
+    height: height,
+    width: width,
+    zIndex: 1,
+    elevation: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  themeSwitcher: {
+    paddingBottom: 10,
+    paddingRight: 4,
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+});
